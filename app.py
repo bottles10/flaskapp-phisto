1d1bf4a6ea415b92bc0fb084650cdea4d2eed7fc
Bottles <henryaovare@gmail.com>
2024-07-06 00:43:13 +0000

    update cors

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from flask_cors import CORS
 from textblob import TextBlob
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/analyze_sentiment": {"origins": "https://bottles10.github.io"}})
+CORS(app)  # Allow all origins
 
 @app.route('/analyze_sentiment', methods=['OPTIONS', 'POST'])
 def analyze_sentiment():
     if request.method == 'OPTIONS':
         # CORS preflight request
         response = jsonify({'status': 'OK'})
-        response.headers.add('Access-Control-Allow-Origin', 'https://bottles10.github.io')
+        response.headers.add('Access-Control-Allow-Origin', '*')
         response.headers.add('Access-Control-Allow-Methods', 'POST')
         response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
         return response
@@ -27,7 +27,7 @@ def analyze_sentiment():
         mood = "Neutral"
 
     response = jsonify({"sentiment": sentiment, "mood": mood})
-    response.headers.add('Access-Control-Allow-Origin', 'https://bottles10.github.io')
+    response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
 if __name__ == '__main__':
